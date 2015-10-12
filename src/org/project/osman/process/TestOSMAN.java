76c269341f973b780b2698c2c8bb48855fb46679
elhaj <elhaj@lancaster.ac.uk>
2015-10-12 18:26:46 +0100

    changed TestOSMAN examples to unicode

--- src/org/project/osman/process/TestOSMAN.java
+++ src/org/project/osman/process/TestOSMAN.java
@@ -12,13 +12,15 @@ public class TestOSMAN {
 		//loading the Arabic dictionary - needed for tokenization and sentence splitting.
 		osman.loadData();
 		
-		String text = "ÇáÓøáÇãõ Úóáóíßõãú æóÑóÍúãóÉõ Çááåö æóÈóÑóßóÇÊõåõ";
-		
+		//text in unicode in case your editor does not support Arabic text
+		//text in Arabic: "ÇáÓøáÇãõ Úóáóíßõãú æóÑóÍúãóÉõ Çááåö æóÈóÑóßóÇÊõåõ"  [set encoding UTF-8 to view this]
+		String text = "\u0627\u0644\u0633\u0651\u0644\u0627\u0645\u064f \u0639\u064e\u0644\u064e\u064a\u0643\u064f\u0645\u0652 \u0648\u064e\u0631\u064e\u062d\u0652\u0645\u064e\u0629\u064f \u0627\u0644\u0644\u0647\u0650 \u0648\u064e\u0628\u064e\u0631\u064e\u0643\u064e\u0627\u062a\u064f\u0647\u064f";
+		System.out.println(text);
 		double osmanScore = osman.calculateOsman(text);
 		System.out.println("Osman Score: " + osmanScore);
 		
 		
-		String textNoTashkeel = "ÇáÓáÇã Úáíßã æÑÍãÉ Çááå æÈÑßÇÊå";
+		String textNoTashkeel = "\u0627\u0644\u0633\u0644\u0627\u0645 \u0639\u0644\u064a\u0643\u0645 \u0648\u0631\u062d\u0645\u0629 \u0627\u0644\u0644\u0647 \u0648\u0628\u0631\u0643\u0627\u062a\u0647";
 
 		String textAddedTashkeel = osman.addTashkeel(textNoTashkeel);
 		//String textAddedTashkeel = osman.addTashkeel(textNoTashkeel);
@@ -26,7 +28,9 @@ public class TestOSMAN {
 		double osmanScore2 = osman.calculateOsman(textAddedTashkeel);
 		System.out.println("Osman Score: " + osmanScore2);
 		
-		String textWithTashkeel  = "ÇáÓøáÇãõ Úóáóíßõãú æóÑóÍúãóÉõ Çááåö æóÈóÑóßóÇÊõåõ";
+		//text in unicode in case your editor does not support Arabic text
+		//text in Arabic: "ÇáÓøáÇãõ Úóáóíßõãú æóÑóÍúãóÉõ Çááåö æóÈóÑóßóÇÊõåõ"  [set encoding UTF-8 to view this]
+		String textWithTashkeel  = "\u0627\u0644\u0633\u0651\u0644\u0627\u0645\u064f \u0639\u064e\u0644\u064e\u064a\u0643\u064f\u0645\u0652 \u0648\u064e\u0631\u064e\u062d\u0652\u0645\u064e\u0629\u064f \u0627\u0644\u0644\u0647\u0650 \u0648\u064e\u0628\u064e\u0631\u064e\u0643\u064e\u0627\u062a\u064f\u0647\u064f";
 		String textTashkeelRemoved = osman.removeTashkeel(textWithTashkeel);
 		System.out.println(textTashkeelRemoved);
 		
